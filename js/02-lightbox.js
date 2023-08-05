a73320fe3cc8efd0890057b7bbdced6e2928615a
andrey0629 <ANDRII.BRIEF@GMAIL.COM>
2023-08-05 11:21:36 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,67 @@
+// Задание 2 - библиотека SimpleLightbox
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, а также пролистывание изображений при помощи клавиатуры. Посмотри демо видео работы галереи с подключенной библиотекой.
+
+// Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
+
+// <li class="gallery__item">
+//    <a class="gallery__link" href="large-image.jpg">
+//       <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+//    </a>
+// </li>
+
+
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+// Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в ul.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const createMarkup = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", createMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        let markup = "";
+        return markup = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="Image description" />
+            </a>
+        </li>`
+    }).join("");
+};
+
+
+const open = (event => {
+    event.preventDefault();
+    const { target } = event;
+    let instance = new SimpleLightbox('.gallery a');
+        instance.on('show.simplelightbox', function () {
+	    // do something…
+        });(`
+        <img src="${target.dataset.source}">
+        `)
+
+    const closeModal = (event => {
+        if (event.code === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', closeModal);
+        }
+    })
+    window.addEventListener('keydown', closeModal);
+});
+
+
+gallery.addEventListener("click", open)
+window.addEventListener("click", close)
+window.addEventListener("keydown", close)
